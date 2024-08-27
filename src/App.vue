66a3c3eb56bb9eac5ec6be1516a3a9a2364feab2
Erica Dsouza <erica.dsouza@sportzinteractive.com>
2024-08-27 17:12:18 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ProductList />
+  </div>
+</template>
+
+<script>
+import ProductList from "./components/ProductsList.vue";
+
+export default {
+  components: { ProductList },
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
